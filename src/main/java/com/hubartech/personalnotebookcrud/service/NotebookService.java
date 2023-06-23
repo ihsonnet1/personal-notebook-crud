c72336c1c6134f3c0373da5b14c3d3f478edc825
Injamamul Haque Sonet <injamamul@swordfishtech.co.uk>
2023-06-24 02:49:06 +0600

    tried mockito test case

--- src/main/java/com/hubartech/personalnotebookcrud/service/NotebookService.java
+++ src/main/java/com/hubartech/personalnotebookcrud/service/NotebookService.java
@@ -4,11 +4,13 @@ import com.hubartech.personalnotebookcrud.dto.BasicDataResponse;
 import com.hubartech.personalnotebookcrud.dto.BasicMessageResponse;
 import com.hubartech.personalnotebookcrud.dto.requests.NotebookRequest;
 import com.hubartech.personalnotebookcrud.dto.responses.NotebookResponse;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-
 public interface NotebookService {
+
     ResponseEntity<BasicMessageResponse> createNote(NotebookRequest notebookResponse);
 
     ResponseEntity<BasicDataResponse<List<NotebookResponse>>> getAllNotes();
